ed3218946eb6d5ea3702fc3079daf52db642793a
yehonathansc <yehonathan.sc@gmail.com>
2025-12-31 13:54:42 +0200

    finishing protocol

--- server_protocol/DNS/client.py
+++ server_protocol/DNS/client.py
@@ -1,10 +1,8 @@
-import time
 from scapy.all import *
 import ipaddress
 from scapy.layers.inet import IP, ICMP
 from typing import Iterable
 
-from scapy.layers.l2 import Ether
 
 MY_IP = get_if_addr(conf.iface)
 DEFAULT_GATEWAY = conf.route.route("0.0.0.0")[2]
@@ -33,14 +31,14 @@ def send_queries(src_ip: str, ips: Iterable):
         packets.append(packet)
     send(packets, verbose=False)
 
-def protocol(mask: int, timeout: float=5):
+def protocol(subnet: str, mask: int, timeout: float=15):
     # 1. Define the sniffer object
     # 'prn' is a callback function for each packet, 'filter' uses BPF syntax
     sniffer = AsyncSniffer(iface=conf.iface, lfilter=filter_packet, count=1, timeout=timeout)
     # 2. Start sniffing (non-blocking)
     sniffer.start()
     
-    send_queries(MY_IP, get_subnet_ips(DEFAULT_GATEWAY, mask))
+    send_queries(MY_IP, get_subnet_ips(subnet, mask))
 
     sniffer.join() # Wait for responses to be captured
     # 3. Stop sniffing when you're done
