75b3353088d9646f8d97ba765aed70e2e1a75daa
Sacharlemagne <sacha.hallermeier@gmail.com>
2026-01-06 00:10:12 +0200

    added server side sniffers

---
+++ python_poc/server/out_sniffer.py
@@ -0,0 +1,80 @@
+from scapy.all import *
+import sys
+
+# --- Configuration ---
+TARGET_SUBNET = "127.16.164.0/24"
+
+# The specific address where the tunneled pings will be sent
+TUNNEL_DEST_IP = "172.16.164.254"
+MY_IP = "172.16.164.94"
+
+def local_ip_to_real(ip):
+    ind = ip.find(".")
+    if ind == -1:
+        raise ValueError("Invalid IP")
+    return MY_IP[:MY_IP.find(".")] + ip[ind:]
+
+
+# Interface to sniff on (e.g., 'eth0', 'wlan0')
+IFACE = "wlp8s0"
+
+
+def encapsulate_and_send(pkt):
+    """
+    Takes the WHOLE packet (headers + data), converts to bytes,
+    and puts it inside an ICMP Echo Request.
+    """
+    try:
+        if IP not in pkt:
+            return
+        # Convert the entire original packet object to raw bytes
+        # This includes IP headers, TCP/UDP headers, and data
+        pkt[IP].dst = local_ip_to_real(pkt[IP].dst)
+        print(f"IP is {pkt[IP].dst}")
+        pkt[IP].src = MY_IP
+        del pkt[IP].chksum
+        del pkt[TCP].chksum
+        full_packet_bytes = bytes(pkt[IP])
+        payload = b"sacha" + full_packet_bytes
+        # Create the tunnel packet
+        # Layer 3: IP (Source=spoofed, Dest=Tunnel Endpoint)
+        # Layer 4: ICMP (Type 8 = Echo Request)
+        # Payload: The raw bytes of the original packet
+        original_dst = pkt[IP].dst
+        tunnel_pkt = IP(dst=TUNNEL_DEST_IP, src=original_dst) / ICMP(type=8) / payload
+
+        # Send without verbose output
+        send(tunnel_pkt, verbose=0, iface=IFACE)
+
+        # Optional: Print summary
+        print(
+            f"[*] Tunneled {len(full_packet_bytes)} bytes intended for {original_dst}")
+
+    except Exception as e:
+        print(f"[!] Error processing packet: {e}")
+
+def main():
+    print(f"[*] Starting Sniffer...")
+    print(f"[*] Targeting Subnet: {TARGET_SUBNET}")
+    print(f"[*] Tunneling to: {TUNNEL_DEST_IP}")
+
+    # --- The BPF Filter ---
+    # 1. 'dst net TARGET_SUBNET': Only capture packets going TO the target subnet
+    # 2. 'and not dst host TUNNEL_DEST_IP': Safety check to prevent loops
+    # 3. 'and not icmp': Optional, to avoid tunneling existing pings (prevents ping loops)
+    bpf_filter = (f"dst host 127.16.164.165 "
+                  f"and src host 127.0.0.1 "
+                  #f"and not dst host {TUNNEL_DEST_IP} "
+                  #f"and not dst host {MY_IP} "
+                  f"and tcp")
+    # Start sniffing
+    # store=0: Do not keep packets in memory (saves RAM)
+    # prn: The callback function to run on every packet
+    sniff(filter=bpf_filter, iface="lo", prn=encapsulate_and_send, store=0)
+
+
+if __name__ == "__main__":
+    # Scapy requires root privileges to sniff and craft packets
+    if os.geteuid() != 0:
+        sys.exit("Please run as root/sudo.")
+    main()
