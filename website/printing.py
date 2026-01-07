32c4cb29943e58f8a9bd01f896171ee5de0d6050
willopen2007 <iftachsamel@mail.tau.ac.il>
2026-01-07 13:06:51 +0200

    added declaration of get_flashed_messages import

--- website/printing.py
+++ website/printing.py
@@ -1,2 +1,13 @@
+import email
+
+
 def add_printing_job(file, time):
-    pass #add cron job and stuff
+    pass  # add cron job and stuff
+
+
+def send_email(attached_file, send_addr, recv_addr):
+    pass  # make a constant for the first two args
+
+
+def handle_schecduled_prints(scheduled_time):
+    pass  # check cron jobs and send emails
