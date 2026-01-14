23b255d0a859bd4fb850443b76b12fd950d72159
yehonathansc <yehonathan.sc@gmail.com>
2026-01-14 14:43:53 +0200

    out sniffer implementation

--- python_poc/server/out_sniffer.py
+++ python_poc/server/out_sniffer.py
@@ -5,16 +5,17 @@ import sys
 
 from scapy.layers.inet import IP, TCP, ICMP
 
-from manager.constants import PAYLOAD_MAGIC
+from python_poc.server.in_sniffer import real_ip_to_local
+from sniff_constants import PAYLOAD_MAGIC
 
 
 class OutSniffer:
 
-    def __init__(self, target_subnet, destination_ip, my_ip, network_interface):
+    def __init__(self, target_subnet, my_ip, network_interface, default_gateway):
         self.target_subnet = target_subnet
-        self.destination_ip = destination_ip
         self.my_ip = my_ip
         self.network_interface = network_interface
+        self.default_gateway = default_gateway
 
     def local_ip_to_real(self, ip):
         ind = ip.find(".")
@@ -43,7 +44,7 @@ class OutSniffer:
             full_packet_bytes = bytes(pkt[IP])
             payload = PAYLOAD_MAGIC + full_packet_bytes
             original_dst = pkt[IP].dst
-            tunnel_pkt = IP(dst=self.destination_ip, src=original_dst) / ICMP(type=8) / payload
+            tunnel_pkt = IP(dst=self.default_gateway, src=original_dst) / ICMP(type=8) / payload
 
             send(tunnel_pkt, verbose=0, iface=self.network_interface)
 
@@ -53,12 +54,11 @@ class OutSniffer:
         except Exception as e:
             print(f"[!] Error processing packet: {e}")
 
-def main(dest_ip, target_subnet, my_ip, network_interface, lo_interface, default_gateway):
+def main(target_subnet, my_ip, network_interface, lo_interface, default_gateway):
     print(f"[*] Starting Sniffer...")
     print(f"[*] Targeting Subnet: {target_subnet}")
-    print(f"[*] Tunneling to: {dest_ip}")
-    sniffer = OutSniffer(target_subnet, dest_ip, my_ip, network_interface)
-    bpf_filter = (f"dst net {target_subnet} and not dst host {default_gateway} and src host 127.0.0.1 and tcp")
+    sniffer = OutSniffer(target_subnet, my_ip, network_interface)
+    bpf_filter = (f"dst net {target_subnet} and not dst host {default_gateway} and src net 127.16.164.0/23 and tcp")
     sniff(filter=bpf_filter, iface=lo_interface, prn=sniffer.encapsulate_and_send, store=0)
 
 
@@ -66,4 +66,4 @@ if __name__ == "__main__":
     if os.geteuid() != 0:
         sys.exit("Please run as root/sudo.")
     args = sys.argv[1:]
-    main(args[0], args[1], args[2], args[3], args[4], args[5])
+    main(args[0], args[1], args[2], args[3], args[4])
