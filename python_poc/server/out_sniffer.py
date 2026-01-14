0a23728199529657fb9ae391d2dc4aef249895a4
StudGinz <ginzburg1@tau.ac.il>
2026-01-14 15:08:50 +0200

    made it eq to other sniffer

--- python_poc/server/out_sniffer.py
+++ python_poc/server/out_sniffer.py
@@ -1,46 +1,48 @@
 import os
+import sys
+import argparse
 
 from scapy.all import  sniff, send
-import sys
-
 from scapy.layers.inet import IP, TCP, ICMP
 
-from python_poc.server.in_sniffer import real_ip_to_local
+from in_sniffer import real_ip_to_local
 from sniff_constants import PAYLOAD_MAGIC
 
 
-class OutSniffer:
+def local_ip_to_real(self, ip):
+    ind = ip.find(".")
+    if ind == -1:
+        raise ValueError("Invalid IP")
+    return self.my_ip[:self.my_ip.find(".")] + ip[ind:]
 
+def real_ip_to_local(self, ip):
+    ind = ip.find(".")
+    if ind == -1:
+        raise ValueError("Invalid IP")
+    return "127" + ip[ind:]
+
+class OutSniffer:
     def __init__(self, target_subnet, my_ip, network_interface, default_gateway):
         self.target_subnet = target_subnet
         self.my_ip = my_ip
         self.network_interface = network_interface
         self.default_gateway = default_gateway
-
-    def local_ip_to_real(self, ip):
-        ind = ip.find(".")
-        if ind == -1:
-            raise ValueError("Invalid IP")
-        return self.my_ip[:self.my_ip.find(".")] + ip[ind:]
-
-
-    def real_ip_to_local(self, ip):
-        ind = ip.find(".")
-        if ind == -1:
-            raise ValueError("Invalid IP")
-        return "127" + ip[ind:]
-
-
+        self.bpf_filter = f"dst net {target_subnet} and tcp"
+    
+    def start_sniff(self):
+        sniff(filter=self.bpf_filter, iface=self.network_interface, prn=self.encapsulate_and_send, store=0)
+    
     def encapsulate_and_send(self, pkt):
         try:
             if IP not in pkt:
                 return
-
-            pkt[IP].dst = self.local_ip_to_real(pkt[IP].dst)
+            pkt[IP].dst = local_ip_to_real(pkt[IP].dst)
             print(f"IP is {pkt[IP].dst}")
             pkt[IP].src = self.my_ip
+            #deleting the checksum to force recalculation
             del pkt[IP].chksum
             del pkt[TCP].chksum
+            #adding into icmp payload, with magic
             full_packet_bytes = bytes(pkt[IP])
             payload = PAYLOAD_MAGIC + full_packet_bytes
             original_dst = pkt[IP].dst
@@ -48,22 +50,25 @@ class OutSniffer:
 
             send(tunnel_pkt, verbose=0, iface=self.network_interface)
 
-            print(
-                f"[*] Tunneled {len(full_packet_bytes)} bytes intended for {original_dst}")
-
         except Exception as e:
-            print(f"[!] Error processing packet: {e}")
+            pass
 
-def main(target_subnet, my_ip, network_interface, lo_interface, default_gateway):
-    print(f"[*] Starting Sniffer...")
-    print(f"[*] Targeting Subnet: {target_subnet}")
-    sniffer = OutSniffer(target_subnet, my_ip, network_interface, default_gateway)
-    bpf_filter = (f"dst net 127.16.164.0/23 and tcp")
-    sniff(filter=bpf_filter, iface=lo_interface, prn=sniffer.encapsulate_and_send, store=0)
+def main():
+    parser = argparse.ArgumentParser(description="Sniffs for ICMP, if its correct magic, injects into lo")
+    parser.add_argument("--our-ip", dest="our_ip", required=True,
+                        help="our IP address to filter")
+    parser.add_argument("--tunnel-dst-ip", dest="tunnel_dst_ip", required=True,
+                        help="Expected source of captured packets")
+    parser.add_argument("--sniffiface", dest="sniffiface", required=True,
+                        help="Network interface to sniff on")
+    parser.add_argument("--sendiface", dest="sendiface", required=True,
+                        help="Network interface to send on (loopback)")
+    args = parser.parse_args()
+    sniffer = OutSniffer(args.our_ip, args.tunnel_dst_ip, args.sniffiface, args.sendiface)
+    sniffer.start_sniff()
 
 
 if __name__ == "__main__":
     if os.geteuid() != 0:
         sys.exit("Please run as root/sudo.")
-    args = sys.argv[1:]
-    main(args[0], args[1], args[2], args[3], args[4])
+    main()
