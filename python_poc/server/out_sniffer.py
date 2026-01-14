3719b46c66f775298cee21d52cb9e35a0aeb9742
yehonathansc <yehonathan.sc@gmail.com>
2026-01-14 14:04:26 +0200

    out sniffer implementation

--- python_poc/server/out_sniffer.py
+++ python_poc/server/out_sniffer.py
@@ -1,49 +1,62 @@
+import os
+
 from scapy.all import  sniff, send
 import sys
 
 from scapy.layers.inet import IP, TCP, ICMP
 
-TARGET_SUBNET = "127.16.164.0/24"
-
-# router
-TUNNEL_DEST_IP = "172.16.164.254"
-MY_IP = "172.16.164.94"
-
-def local_ip_to_real(ip):
-    ind = ip.find(".")
-    if ind == -1:
-        raise ValueError("Invalid IP")
-    return MY_IP[:MY_IP.find(".")] + ip[ind:]
+from python_poc.server.sniff_constants import PAYLOAD_MAGIC
 
 
-# wifi interface
-IFACE = "wlp8s0"
+class out_sniffer:
+
+    def __init__(self, target_subnet, destination_ip, my_ip, network_interface):
+        self.target_subnet = target_subnet
+        self.destination_ip = destination_ip
+        self.my_ip = my_ip
+        self.network_interface = network_interface
+
+    TARGET_SUBNET = "127.16.164.0/24"
+
+    # router
+    TUNNEL_DEST_IP = "172.16.164.254"
+    MY_IP = "172.16.164.94"
+
+    def local_ip_to_real(self, ip):
+        ind = ip.find(".")
+        if ind == -1:
+            raise ValueError("Invalid IP")
+        return self.my_ip[:self.my_ip.find(".")] + ip[ind:]
 
 
-def encapsulate_and_send(pkt):
-    try:
-        if IP not in pkt:
-            return
+    # Wi-Fi interface
+    # IFACE = "wlp8s0"
 
-        pkt[IP].dst = local_ip_to_real(pkt[IP].dst)
-        print(f"IP is {pkt[IP].dst}")
-        pkt[IP].src = MY_IP
-        del pkt[IP].chksum
-        del pkt[TCP].chksum
-        full_packet_bytes = bytes(pkt[IP])
-        payload = b"sacha" + full_packet_bytes
-        original_dst = pkt[IP].dst
-        tunnel_pkt = IP(dst=TUNNEL_DEST_IP, src=original_dst) / ICMP(type=8) / payload
 
-        send(tunnel_pkt, verbose=0, iface=IFACE)
+    def encapsulate_and_send(self, pkt):
+        try:
+            if IP not in pkt:
+                return
 
-        print(
-            f"[*] Tunneled {len(full_packet_bytes)} bytes intended for {original_dst}")
+            pkt[IP].dst = self.local_ip_to_real(pkt[IP].dst)
+            print(f"IP is {pkt[IP].dst}")
+            pkt[IP].src = self.my_ip
+            del pkt[IP].chksum
+            del pkt[TCP].chksum
+            full_packet_bytes = bytes(pkt[IP])
+            payload = PAYLOAD_MAGIC + full_packet_bytes
+            original_dst = pkt[IP].dst
+            tunnel_pkt = IP(dst=self.destination_ip, src=original_dst) / ICMP(type=8) / payload
 
-    except Exception as e:
-        print(f"[!] Error processing packet: {e}")
+            send(tunnel_pkt, verbose=0, iface=self.network_interface)
 
-def main():
+            print(
+                f"[*] Tunneled {len(full_packet_bytes)} bytes intended for {original_dst}")
+
+        except Exception as e:
+            print(f"[!] Error processing packet: {e}")
+
+def main(TUNNEL_DEST_IP, TARGET_SUBNET, my_ip, network_interface):
     print(f"[*] Starting Sniffer...")
     print(f"[*] Targeting Subnet: {TARGET_SUBNET}")
     print(f"[*] Tunneling to: {TUNNEL_DEST_IP}")
