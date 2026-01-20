a24ab0b195fe0aab117cb19fa0dd01fc77cbb52d
StudGinz <ginzburg1@tau.ac.il>
2026-01-20 17:39:51 +0200

    created working filter

--- python_poc/server/in_sniffer.py
+++ python_poc/server/in_sniffer.py
@@ -11,6 +11,13 @@ def real_ip_to_local(ip):
         raise ValueError("Invalid IP")
     return "127" + ip[ind:]
 
+def real_subnet_to_local(ip):
+    ind = ip.find(".")
+    last_ind = ip.rfind(".")
+    if ind == -1 or last_ind == -1:
+        raise ValueError("Invalid IP")
+    return "127" + ip[ind:last_ind]+".0"
+
 class Sniffer:
     def __init__(self, our_ip, default_gateway, sniff_iface, lo_iface):
         self.our_ip = our_ip
