baae3a4f75bf814753ad29aaa4a802aea857ba45
StudGinz <ginzburg1@tau.ac.il>
2026-01-21 12:50:28 +0200

    did main function, added docs

--- linux on connect/disableRST.py
+++ linux on connect/disableRST.py
@@ -1,7 +1,9 @@
 import subprocess
 
 def disable_rst():
+    
     disable_recv = "sudo iptables -A OUTPUT -p tcp --tcp-flags RST RST -d 127.16.164.0/23 -j DROP"
-    disable_send = "sudo iptables -A OUTPUT -s 127.16.164.0/23 -p tcp --tcp-flags RST RST -j DROP"
     subprocess.run(disable_recv, shell=True, check=True)
+
+    disable_send = "sudo iptables -A OUTPUT -s 127.16.164.0/23 -p tcp --tcp-flags RST RST -j DROP"
     subprocess.run(disable_send, shell=True, check=True)
--- linux on connect/onConnectScript.py
+++ linux on connect/onConnectScript.py
@@ -1,10 +1,7 @@
 import disableRST
-
-def main():
+import editRouting
+def on_connect():
+    #disables TCP RST packets, because the OS gets confused and misleads clients to think ports are closed
     disableRST.disable_rst()
-
-
-
-
-if __name__ == "__main__":
-    main()
+    #Edits the routing table so 127.x.x.x will go through loopback, to be picked up by our sniffers
+    editRouting.edit_routing()
