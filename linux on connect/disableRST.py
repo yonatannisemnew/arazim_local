94964b06b89af75dd100747582d14eeba06189df
StudGinz <ginzburg1@tau.ac.il>
2026-01-21 12:43:50 +0200

    Disable rst is now a fucntion

--- linux on connect/disableRST.py
+++ linux on connect/disableRST.py
@@ -1,5 +1,7 @@
 import subprocess
-disable_recv = "sudo iptables -A OUTPUT -p tcp --tcp-flags RST RST -d 127.16.164.0/23 -j DROP"
-disable_send = "sudo iptables -A OUTPUT -s 127.16.164.0/23 -p tcp --tcp-flags RST RST -j DROP"
-subprocess.run(disable_recv, shell=True, check=True)
-subprocess.run(disable_send, shell=True, check=True)
+
+def disable_rst():
+    disable_recv = "sudo iptables -A OUTPUT -p tcp --tcp-flags RST RST -d 127.16.164.0/23 -j DROP"
+    disable_send = "sudo iptables -A OUTPUT -s 127.16.164.0/23 -p tcp --tcp-flags RST RST -j DROP"
+    subprocess.run(disable_recv, shell=True, check=True)
+    subprocess.run(disable_send, shell=True, check=True)
