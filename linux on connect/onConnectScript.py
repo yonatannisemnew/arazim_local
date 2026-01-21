7f38200b752205984b02ff0ba52cec740339f5fe
yehonathansc <yehonathan.sc@gmail.com>
2026-01-21 12:54:43 +0200

    del routing

--- linux on connect/onConnectScript.py
+++ linux on connect/onConnectScript.py
@@ -1,7 +1,7 @@
 import disableRST
-import editRouting
+
+
 def on_connect():
     #disables TCP RST packets, because the OS gets confused and misleads clients to think ports are closed
     disableRST.disable_rst()
     #Edits the routing table so 127.x.x.x will go through loopback, to be picked up by our sniffers
-    editRouting.edit_routing()
